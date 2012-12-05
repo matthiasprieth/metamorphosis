2d70378f122b31bf2675ca206d0a88c8b4af6edb
Manuel Mitterer <mmitterer.mmt-b2011@fh-salzburg.ac.at>
2012-12-05 16:28:12 +0100

    gemfile.lock readded

---
+++ Capfile
@@ -0,0 +1,16 @@
+load 'deploy'
+# Uncomment if you are using Rails' asset pipeline
+    # load 'deploy/assets'
+load 'config/deploy' # remove this line to skip loading any of the default tasks
+
+task :display_free_disk_space do
+  run "df -h"
+end
+
+namespace :deploy do
+	task :linkconfig do
+		run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+	end
+end
+require "bundler/capistrano"
+after "deploy:update_code", "deploy:linkconfig"
---
+++ config/deploy.rb
@@ -0,0 +1,14 @@
+ require "bundler/capistrano"
+ set :application, "metamorphosis"
+ set :scm, :git
+ set :repository,  "ssh://fhs33729@repos.mediacube.at/opt/git/web_2012/metamorphosis.git/"
+ set :deploy_to, "/var/www/virthosts/metamorphosis.mediacube.at"
+ set :user, "deploy_metam"
+ set :branch, "master"
+ default_run_options[:pty] = true
+ set :use_sudo, false
+ set :port, 5412
+ role :web, "multimediaart.at"                   # Your HTTP server, Apache/etc
+ role :app, "multimediaart.at"                   # This may be the same as your `Web` server
+ role :db,  "multimediaart.at", :primary => true # This is where Rails migrations will run
+ # role :db,  "your slave db-server here
