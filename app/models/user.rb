96f384ec5f36dda29f5dbfc6e24ff5b014d1d68a
Matthias Prieth <mprieth.mmt-b2011@fh-salzburg.ac.at>
2012-12-07 20:27:03 +0100

    added basic fb-login(ssl missing)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,  :omniauthable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :provider, :uid
   attr_accessible :description, :email, :firstname, :lastname, :password, :salt
   has_many :pictures
   has_many :comments
   has_and_belongs_to_many :rewards
+  validates_presence_of :username
+
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(username:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+      )
+    end
+    user.skip_confirmation!
+    user
+  end
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,6 +15,10 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
+  require "omniauth-facebook"
+  config.omniauth :facebook, "315173175265453", "2645c64f28cbf8eef8a1078d16a795ff", :client_options => {
+  :ssl => {:verify => false}}
+
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
