1c87fa156d03aded44fd2d408f190c8dfdd9f10a
Manuel Mitterer <mmitterer.mmt-b2011@fh-salzburg.ac.at>
2013-01-05 15:46:52 +0100

    minor fixed to comments, can only be deleted and updated by the creator

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: [:index, :show, :download]
   
   require 'mime/types'
   # GET /pictures
