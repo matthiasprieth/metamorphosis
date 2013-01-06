f995e739e767e75fd0876d42118eee2f40375aae
Manuel Mitterer <mmitterer.mmt-b2011@fh-salzburg.ac.at>
2013-01-06 21:48:24 +0100

    minor fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Metamorphosis::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this) --> wtf Apache doesn't do anything
   #config.serve_static_assets = false
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
