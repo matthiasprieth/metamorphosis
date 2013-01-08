85ce5194991f1a63e0b52c853f96f6fc94706fd7
Matthias Prieth <mprieth.mmt-b2011@fh-salzburg.ac.at>
2013-01-08 17:00:53 +0100

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -86,7 +86,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
 
-    Picture.find(:all, @user).each do |picture|
+    Picture.find_all_by_id(@user).each do |picture|
       picture.destroy
     end
 
